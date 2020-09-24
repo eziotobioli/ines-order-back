b0faed19fa44383a6de52a0404f05c5edbb15b9a
ezio tobioli <eziotobioli@tacto.group>
2020-09-23 22:15:32 -0300

    initial commit

---
+++ src/main/java/cl/cafeines/web/menu/demo/WebMenuApplication.java
@@ -0,0 +1,13 @@
+package cl.cafeines.web.menu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/cafeines/web/menu/demo/WebMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.cafeines.web.menu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
